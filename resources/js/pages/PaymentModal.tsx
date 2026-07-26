e5753b1b3abdec3aba36c87f80850b643d0d12c1
hameed-fu <hameedawkumcs@gmail.com>
2026-07-26 17:30:46 +0500

    updates

--- resources/js/pages/PaymentModal.tsx
+++ resources/js/pages/PaymentModal.tsx
@@ -86,8 +86,13 @@ export default function PaymentModal({
                 )}
                 Pay with PayPal
               </Button>
-              <div className="text-red-500 mt-2">
-                You do not need a PayPal account to make a payment using PayPal
+              <div className="flex items-start gap-2 bg-blue-50 border border-blue-200 rounded-lg p-3 mt-2">
+                <svg className="w-4 h-4 text-blue-500 mt-0.5 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span className="text-sm text-blue-700 leading-tight">
+                  You can check out as a guest if you do not have a PayPal account
+                </span>
               </div>
             </div>
 
